aef5e5df32088a89d7f69bbafe397be6cd854721
Dmytro Stepanenko <dmitriy.s.stepanenko@gmail.com>
2023-12-05 19:45:17 +0200

    update app settings

---
+++ src/test/java/com/todo_list/backendspringboot/BackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo_list.backendspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
